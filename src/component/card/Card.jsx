3925c1156a7f0bc446a16e2c5c9ec76a098260f3
sabila111 <sharminsharker2002@gmail.com>
2023-09-27 21:50:51 +0600

    finished all work

--- src/component/card/Card.jsx
+++ src/component/card/Card.jsx
@@ -6,7 +6,7 @@ const Card = ({cards}) => {
     return (
         <div>
 
-            <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-5 mt-10 ">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-10 md:gap-15 lg:gap-5 mt-10   ">
                   {
                 cards.map(card=> <DonationCarddd key={card.id} card={card} ></DonationCarddd>)
                   }
